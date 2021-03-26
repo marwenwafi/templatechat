ebdf4f6ece98916b89058b166f8814d56935e14a
marwenwafi <marwen.wafi@esprit.tn>
2021-03-26 15:45:20 +0100

    initial Commit

---
+++ src/Layout/Navleft.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react'  
+  
+export class Navleft extends Component {  
+    render() {  
+        return (<>
+        <aside class="main-sidebar">
+    
+    <section class="sidebar">	
+      
+      <ul class="sidebar-menu" data-widget="tree">
+		
+		<li class="treeview">
+          <a href="#">
+			<img src="https://www.multipurposethemes.com/admin/chat-bot-admin-template/images/svg-icon/sidebar-menu/dashboard.svg" class="svg-icon" alt=""></img>
+            <span>Dashboard</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="index.html"><i class="ti-more"></i>Dashboard 1</a></li>
+          </ul>
+        </li>   		  
+		
+        <li class="header">Apps  UI</li>
+		
+	
+		  
+		<li class="treeview">
+          <a href="#">
+            <img src="https://www.multipurposethemes.com/admin/chat-bot-admin-template/images/svg-icon/sidebar-menu/apps.svg" class="svg-icon" alt=""></img>
+			<span>Apps</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="extra_taskboard.html"><i class="ti-more"></i>Todo</a></li>
+            <li><a href="extra_calendar.html"><i class="ti-more"></i>Calendar</a></li>
+          </ul>
+        </li>			  
+		         
+		
+		 		  
+        
+		
+	 
+		  
+		
+        <li class="header">Pages</li> 	
+		  
+		<li class="treeview">
+          <a href="#">
+            <img src="https://www.multipurposethemes.com/admin/chat-bot-admin-template/images/svg-icon/sidebar-menu/pages.svg" class="svg-icon" alt=""></img>
+            <span>User Pages</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            
+			<li><a href="extra_profile.html"><i class="ti-more"></i>User Profile</a></li>
+			
+          </ul>
+        </li>
+			  
+		  
+		<li class="treeview">
+          <a href="#">
+            <img src="https://www.multipurposethemes.com/admin/chat-bot-admin-template/images/svg-icon/sidebar-menu/miscellaneous.svg" class="svg-icon" alt=""></img>
+			<span>Miscellaneous</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+			<li><a href="error_404.html"><i class="ti-more"></i>Error 404</a></li>
+			<li><a href="error_500.html"><i class="ti-more"></i>Error 500</a></li>
+			<li><a href="error_maintenance.html"><i class="ti-more"></i>Maintenance</a></li>	
+          </ul>
+        </li>
+		  
+		<li class="header">Charts  Maps</li>		  
+		
+		<li class="treeview">
+          <a href="#">
+            <img src="https://www.multipurposethemes.com/admin/chat-bot-admin-template/images/svg-icon/sidebar-menu/charts.svg" class="svg-icon" alt=""/>
+			<span>Charts</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="charts_chartjs.html"><i class="ti-more"></i>ChartJS</a></li>
+            
+          </ul>
+        </li>		  
+		
+		<li class="treeview">
+          <a href="#">
+            <img src="https://www.multipurposethemes.com/admin/chat-bot-admin-template/images/svg-icon/sidebar-menu/maps.svg" class="svg-icon" alt=""/>
+			<span>Maps</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            
+            <li><a href="map_vector.html"><i class="ti-more"></i>Vector Map</a></li>
+          </ul>
+        </li>
+		
+	
+      </ul>
+    </section>
+  </aside>
+         </>)}}
+export default Navleft;
